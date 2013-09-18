c6d217b31572aed677d9e56dae935b53bcd2c607
Jean-Philippe Caissy <jpcaissy@piji.ca>
2013-09-18 12:35:39 -0400

    Switched readline example for lecture de fichier

---
+++ labo-02/lecture-fichier.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+
+/*
+ * Ne jamais utiliser les métohdes synchrones !
+ */
+fs.writeFileSync('fichier', 'Cette chaine est dans le fichier');
+
+fs.readFile('fichier', function(err, data) {
+  if(err) {
+    throw err;
+  }
+  console.log('Callback de la lecture du fichier');
+  console.log(data.toString());
+});
+
+console.log('Fin du fichier javascript');
