bf1e37a08dc473a7de0ecd1af274365c3082626c
Jean-Philippe Caissy <jpcaissy@piji.ca>
2013-09-18 12:40:27 -0400

    Updated l'exmple du labo 2

--- labo-02/lecture-fichier.js
+++ labo-02/lecture-fichier.js
@@ -1,16 +1,16 @@
-var fs = require('fs');
+    var fs = require('fs');
 
-/*
- * Ne jamais utiliser les métohdes synchrones !
- */
-fs.writeFileSync('fichier', 'Cette chaine est dans le fichier');
+    /*
+     * Ne jamais utiliser les métohdes synchrones !
+     */
+    fs.writeFileSync('fichier', 'Cette chaine est dans le fichier');
 
-fs.readFile('fichier', function(err, data) {
-  if(err) {
-    throw err;
-  }
-  console.log('Callback de la lecture du fichier');
-  console.log(data.toString());
-});
+    fs.readFile('fichier', function(err, data) {
+      if(err) {
+        throw err;
+      }
+      console.log('Callback de la lecture du fichier');
+      console.log(data.toString());
+    });
 
-console.log('Fin du fichier javascript');
+    console.log('Fin du fichier javascript');
